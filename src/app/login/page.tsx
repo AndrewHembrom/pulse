fa490fa3d2562af7174ea61b7202df1fa6d75776
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2025-05-21 18:46:57 +0530

    Supabase stuff

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Authform from "@/components/Authform";
 
 export default function LoginPage() {
   return (
-    <div className="mt-20 flex-1 flex-col items-center">
+    <div className="mt-20 flex flex-1 flex-col items-center">
       <Card className="w-full max-w-md">
         <CardHeader className="mb-4">
           <CardTitle className="text-center text-3xl">Login</CardTitle>
